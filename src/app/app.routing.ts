e9bb02cd63e5e3f570acdf6f6e964d441b25724f
Baskaran R <baskaran.ramalingam@theoptimum.net>
2016-11-21 18:21:40 +0800

    check router path match full

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '**', component: PageNotFoundComponent }
